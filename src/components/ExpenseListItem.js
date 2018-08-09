a0bad5255024e69055b63b53017cb84edbaeebd2
Homere Faivre <Homy@Homeres-MacBook-Pro.local>
2018-08-09 15:02:59 +0200

    Setting up currency formating

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import numeral from 'numeral';
+import "numeral/locales/fr";
 
+
+numeral.locale('fr');
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
     </Link>
-    <p>{amount} - Created At: {moment(createdAt).format('MMM Do, YYYY')}</p>
+    <p>{numeral(amount/100).format('$0,0.00')}
+       -
+       Created At: {moment(createdAt).format('MMM Do, YYYY')}
+    </p>
   </div>
 );
 
