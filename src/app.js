ed33dd7780bbc1e740262bb5eb824164ee78f03a
Homere Faivre <Homy@Homeres-MacBook-Pro.local>
2018-08-09 11:20:08 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+import {fakeExpenses} from './seeders/seedExpenses';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
+
+const store = configureStore();
+
+//Seeding with fake data
+const expenses = fakeExpenses;
+
+expenses.forEach((expense)=>{
+	store.dispatch(addExpense(expense));
+});
+
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
---
+++ src/seeders/seedExpenses.js
@@ -0,0 +1,47 @@
+import faker from 'faker';
+import uuid from 'uuid';
+
+const descriptions = [
+	"Lunch @ Jimmy's",
+	"Gass Bill",
+	"Bicycle Parts",
+	"Trip to Brussels",
+	"Petrol",
+	"Coffee at Starbucks",
+	"Mission Impossible movie Ticket",
+	"Netflix Subscription",
+	"Guitar lesson",
+	"Groceries",
+	"Bicycle Paint Job",
+	"Saint Valentines restaurants",
+	"Concert tickets for Snarky Puppy",
+	"Phone charger",
+	"Taxi from Airport to home",
+	"Lunch with Client",
+	"2 pint at les Chaises",
+	"New shoes", 
+	"Burger king"
+];
+
+
+export const fakeExpenses = descriptions.map((expense)=>{
+	return {
+		description : expense,
+		amount : Math.floor(faker.finance.amount()*100),
+		note: faker.hacker.phrase(),
+		createdAt : faker.date.recent().valueOf()
+	}
+});
+
+
+
+export const fakeExpensesWithId = fakeExpenses.map((expense)=>{
+	return {
+		id: uuid(),
+		...expense
+	}
+});
+
+
+
+
